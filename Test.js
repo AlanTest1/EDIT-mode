006ced444c670703f14978d1aa4693c0a9ef6385
TesterAlanQA <94017257+TesterAlanQA@users.noreply.github.com>
2022-03-17 18:03:35 -0700

    test

--- Test.js
+++ Test.js
@@ -4,4 +4,4 @@ intent('hello world', p => {
 });
 
 
-//hi I am a comment some new changes in the script//hi I am a comment :)//hi//hi I//hi I am a comment :) am a comment :) I//hi I am a comment :)//hi I am a comment :)//hi I am a comment :) am a comment :)
+//hi I am a comment :)
